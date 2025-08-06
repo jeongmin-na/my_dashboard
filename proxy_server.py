9e66ff56876612ab80880368ebd1b9485ec8cd89
jeongmin-na <jeongmin.na7@gmail.com>
2025-08-06 15:28:25 +0900

    Update proxy_server.py

--- proxy_server.py
+++ proxy_server.py
@@ -265,4 +265,9 @@ if __name__ == "__main__":
         start_dashboard()
     else:
         # 일반적인 프록시 서버 실행
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
         run_proxy_server() 
